ed83bfeeccf2eb37b2e887ce4155c53704734273
Victor Juan Jimenez Flores <vj.jimenez96@gmail.com>
2020-02-17 01:19:13 -0500

    added bcryptjs

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,6 +1,6 @@
 const User = require("../models/Users.js");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const config = require("../config.js");
 
 const logout = (req, res) => {

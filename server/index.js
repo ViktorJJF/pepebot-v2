5ced65036c4b353d7263f1435de502fda0717ccd
ViktorJJF <vj.jimenez96@gmail.com>
2022-01-05 05:42:08 -0500

    new features

--- server/index.js
+++ server/index.js
@@ -97,25 +97,25 @@ BotModel.find().exec(async (err, payload) => {
 
     bot.initialize(element);
     bots.addBot(bot);
-    if (bot.ogameEmail != "carlos.jf.1681@gmail.com") {
-      console.log("empezando login", bot.ogameEmail);
-      let login = await bot.login(element.ogameEmail, element.ogamePassword);
-      // console.log("🚀 Aqui *** -> login", login);
-      console.log("se termino el loginnnn");
-      // const beginSpies = require("./ogameScripts/spyRange");
-      // await beginSpies(bot, "2:104", "2:300", "moon");
-      // const spyPlayer = require("./ogameScripts/spyPlayer");
-      // await spyPlayer(bot, "Constable Zenith");
-      // // await timeout(10 * 1000);
-      // let page = await bot.createNewPage();
-      // await bot.filterSpyMessages(page);
-      // const { sendAllShipsToDebris } = require("./ogameScripts/scripts");
-      // await sendAllShipsToDebris("2:111:10", 0.1, page, "planet");
-      if (login) {
-        beginActions(bot);
-      }
-      //daily rutine
+    // if (bot.ogameEmail != "carlos.jf.1681@gmail.com") {
+    console.log("empezando login", bot.ogameEmail);
+    let login = await bot.login(element.ogameEmail, element.ogamePassword);
+    // console.log("🚀 Aqui *** -> login", login);
+    console.log("se termino el loginnnn");
+    // const beginSpies = require("./ogameScripts/spyRange");
+    // await beginSpies(bot, "2:104", "2:300", "moon");
+    // const spyPlayer = require("./ogameScripts/spyPlayer");
+    // await spyPlayer(bot, "Constable Zenith");
+    // // await timeout(10 * 1000);
+    // let page = await bot.createNewPage();
+    // await bot.filterSpyMessages(page);
+    // const { sendAllShipsToDebris } = require("./ogameScripts/scripts");
+    // await sendAllShipsToDebris("2:111:10", 0.1, page, "planet");
+    if (login) {
+      beginActions(bot);
     }
+    //daily rutine
+    // }
   });
   // beginDailyFleetSave(bots.bots);
   // seed.actions();

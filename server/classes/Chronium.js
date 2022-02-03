f8897a959c80ce6507ecd6f707a6b8a74ebb2d39
ViktorJJF <vj.jimenez96@gmail.com>
2022-02-03 14:49:44 -0500

    deleted executable path

--- server/classes/Chronium.js
+++ server/classes/Chronium.js
@@ -16,8 +16,6 @@ class Chronium {
     } else {
       browser = await puppeteer.launch({
         headless: true,
-        // executablePath: "/usr/bin/firefox",
-        executablePath: "/usr/bin/chromium-browser",
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
     }
